adc3ab49db404387e3c67d962faeb7237ff44f33
krstn177 <58555594+krstn177@users.noreply.github.com>
2022-05-21 15:31:29 +0200

    Add files via upload

---
+++ practice1/MultiplicationTable.js
@@ -0,0 +1,6 @@
+function multiplication(parameter){
+    for(let i = 1; i <= 10; i++){
+        let sequence = parameter + ' ' + 'X' + ' ' + i + ' ' + '='+ ' '+ parameter*i;
+        console.log(sequence);
+    }   
+}
