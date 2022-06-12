5c0ca5a4f8d717a13f90949bf3619d5dad08e612
krstn177 <58555594+krstn177@users.noreply.github.com>
2022-06-12 17:35:52 +0200

    Add files via upload

---
+++ practice4/SmallestNumbers.js
@@ -0,0 +1,14 @@
+function smallestNumber(num1, num2, num3) {
+    let smallestNumber = function (num1, num2, num3) {
+        let smallestNumber = 0;
+        if (num1 < num2 && num1 < num3) {
+            smallestNumber = num1;
+        } else if (num2 < num1 && num2 < num3) {
+            smallestNumber = num2
+        } else {
+            smallestNumber = num3;
+        }
+        return smallestNumber;
+    }
+    console.log(smallestNumber(num1, num2, num3));
+}
