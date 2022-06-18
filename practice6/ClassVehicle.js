4608631184403e07d12b6198a79cd45540e2c6db
krstn177 <58555594+krstn177@users.noreply.github.com>
2022-06-18 14:53:54 +0200

    Add files via upload

---
+++ practice6/ClassVehicle.js
@@ -0,0 +1,16 @@
+class Vehicle{
+    constructor(type, model, parts, fuel){
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.parts.quality = this.parts.engine*this.parts.power;
+        this.fuel = fuel;
+    }
+    drive(consumption){
+        this.fuel -= consumption;
+    }
+}
+let parts = {engine: 9, power: 500};
+let vehicle = new Vehicle('l', 'k', parts, 840);
+vehicle.drive(20);
+console.log(vehicle.fuel);
