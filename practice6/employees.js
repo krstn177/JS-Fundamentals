e141959bb414b8698c18493ee38ddaa76e752f08
krstn177 <58555594+krstn177@users.noreply.github.com>
2022-06-20 11:32:36 +0200

    Add files via upload

---
+++ practice6/employees.js
@@ -0,0 +1,9 @@
+function employees(input) {
+    let listOfEmployees = {};
+    for (let employee of input) {
+        listOfEmployees.name = employee;
+        listOfEmployees.personalNumber = employee.length;
+        console.log(`Name: ${listOfEmployees.name} -- Personal Number: ${listOfEmployees.personalNumber}`);
+    }
+}
+employees(['Silas Butler','Adnaan Buckley','Juan Peterson','Brendan Villarreal']);
