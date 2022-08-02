b109ad62e5cb193513abe452becef598288abd2a
krstn177 <58555594+krstn177@users.noreply.github.com>
2022-08-02 21:13:18 +0200

    Add files via upload

---
+++ practice11/GuineaPig.js
@@ -0,0 +1,41 @@
+function guineaPig(input) {
+  let food = input.shift() * 1000;
+  let hay = input.shift() * 1000;
+  let cover = input.shift() * 1000;
+  let weight = input.shift() * 1000;
+  for (let index = 1; index <= 30; index++) {
+    food -= 300;
+    if (index % 2 == 0) {
+      hay -= 0.05 * food;
+    }
+    if (index % 3 == 0) {
+      cover -= weight / 3;
+    }
+  }
+  if (food <= 0 || hay <= 0 || cover <= 0) {
+    console.log("Merry must go to the pet store!");
+  } else {
+    console.log(
+      `Everything is fine! Puppy is happy! Food: ${(food/1000).toFixed(
+        2
+      )}, Hay: ${(hay/1000).toFixed(2)}, Cover: ${(cover/1000).toFixed(2)}.`
+    );
+  }
+}
+guineaPig((["10", 
+"5", 
+"5.2", 
+"1"])
+);
+guineaPig(["1", 
+"1.5", 
+"3", 
+"1.5"
+]
+);
+guineaPig(["9",
+"5",
+"5.2",
+"1"]);
+
+
